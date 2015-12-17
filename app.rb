e15e883ce6c227c02e7b57e4c26c7cb218bc2188
Don Brower <dbrower@nd.edu>
2015-12-17 15:49:42 -0500

    Add DOIs

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ solr = RSolr.connect :url => ENV['SOLR_URL']
 
 def process_citation(style, item)
   cp = CiteProc::Processor.new style: style, format:'html'
+  cp.engine.format = 'html'
   cp << item
-  r = cp.render :bibliography, id: item.id
+  r = cp.render :bibliography, id: item.id, format: 'html'
   # for some reason render returns a list
   r.first
 end
@@ -37,16 +38,18 @@ get '/citation/:id' do |id|
   end
 
   solr_record = response["docs"].first
+  dois = solr_record["desc_metadata__identifier_tesim"]
+  doi = nil
+  if dois && dois.length > 0
+    doi = dois.first.sub(/\A.*?10/,"10")
+  end
   item = CiteProc::Item.new(
     id: id,
     URL: "https://curate.nd.edu/show/#{id}",
     title: solr_record["desc_metadata__title_tesim"].first,
     author: solr_record["desc_metadata__creator_tesim"],
+    DOI: doi,
   )
-#  dois = solr_record["desc_metadata__identifier_tesim"]
-#  if dois && dois.length > 0
-#    item.DOI = dois.first
-#  end
 
   result = {}
   STYLES.map do |style|

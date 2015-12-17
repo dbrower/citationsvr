5de0c4d35ee9735409e416b59530abef17aed20d
Don Brower <dbrower@nd.edu>
2015-12-17 13:22:49 -0500

    Add root path redirect to index.html

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@
 require 'sinatra'
 require 'json'
 
+
 get '/c/:id' do |id|
   "Hello world! #{id}"
   {"id" => id,
    "ala" => "Some html string. <i>with formatting</i>"}.to_json
+end
+
+get '/' do
+  redirect to('index.html')
 end
